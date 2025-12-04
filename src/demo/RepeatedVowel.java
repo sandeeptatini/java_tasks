25788759abec518e2fbb77fb270ba89bbf91a91a
sandeeptatini <21jr1a04i3@gmail.com>
2025-12-04 12:13:16 +0530

    updatedchanges

--- src/demo/RepeatedVowel.java
+++ src/demo/RepeatedVowel.java
@@ -4,17 +4,10 @@ public class RepeatedVowel {
     public static void main(String[] args) {
         String s = "aeiouAEIOUaeiouAEIOU";
         String str = s.toLowerCase();
-
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-
-            // check only vowels
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-
-                // first occurrence check
                 if (str.indexOf(c) == i) {
-
-                    // check if the vowel appears again
                     if (str.indexOf(c, i + 1) != -1) {
                         System.out.print(c);
                     }
@@ -23,3 +16,4 @@ public class RepeatedVowel {
         }
     }
 }
+									
